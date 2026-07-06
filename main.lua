296f0f7c29d850bd5f47956f050325981b82d6a6
BeginnerLevelDev <gtalover3@proton.me>
2026-07-07 00:04:23 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,12 +9,10 @@ local Window = Rayfield:CreateWindow({
 })
 
 -- Load Tab 1 module and pass the Window to it
-local loadTab1 = loadstring(game:HttpGet("https://raw.githubusercontent.com/BeginnerLevelDev/brainrotsestroyer/refs/heads/main/script1.lua"))()
-loadTab1(Window)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BeginnerLevelDev/brainrotsestroyer/refs/heads/main/script1.lua"))(Window)
 
 -- Load Tab 2 module and pass the Window to it
-local loadTab2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/BeginnerLevelDev/brainrotsestroyer/refs/heads/main/movement.lua"))()
-loadTab2(Window)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BeginnerLevelDev/brainrotsestroyer/refs/heads/main/movement.lua"))(Window)
 
 -- Final notification when everything is loaded
 Rayfield:Notify({ Name = "Loaded!", Content = "All tabs organized and ready.", Duration = 3
