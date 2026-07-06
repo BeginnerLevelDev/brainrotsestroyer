b070f6f3d9dc3c2c75d342de638cd32215982bca
BeginnerLevelDev <gtalover3@proton.me>
2026-07-06 23:54:28 +1000

    Create movement.lua

---
+++ movement.lua
@@ -0,0 +1,17 @@
+-- Return a function so the main file can pass the Window object
+return function(Window)
+    -- Create the Movement Tab using the passed Window
+    local MovementTab = Window:CreateTab("Movement", 4483362458)
+
+    MovementTab:CreateSlider({
+       Name = "WalkSpeed",
+       Range = {16, 200},
+       Increment = 1,
+       Suffix = "Speed",
+       CurrentValue = 16,
+       Flag = "Slider1", 
+       Callback = function(Value)
+           game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+       end,
+    })
+end
